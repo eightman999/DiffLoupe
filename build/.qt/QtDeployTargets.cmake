c6931805b3f828e9fa0ba71931236f4ff0c2abfc
eightman <eightman124@gmail.com>
2025-07-14 23:27:53 +0900

    Delete build directory
    ビルドは要らないです

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_DiffLoupe_FILE /Users/eightman/Desktop/DiffLoupe/build/DiffLoupe.app/Contents/MacOS/DiffLoupe)
-set(__QT_DEPLOY_TARGET_DiffLoupe_TYPE EXECUTABLE)
--- build/CMakeFiles/DiffLoupe.dir/DiffLoupe_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/DiffLoupe.dir/DiffLoupe_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
-  /Users/eightman/Desktop/DiffLoupe/build/DiffLoupe_autogen/3YJK5W5UP7/qrc_resources.cpp
